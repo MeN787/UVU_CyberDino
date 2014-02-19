f7be07c548362cf3a229a797ab123fad0e422fd3
SamSpray <Samantha.Spray@gmail.com>
2014-02-19 09:37:03 -0700

    Adding my project to my branch
    My individual project files to work with and add to the master branch
    eventually.

---
+++ CyberDino_Racing(GitHub)/Assets/Racers/Scripts/DinoRagdoll.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DinoRagdoll : MonoBehaviour {
+
+	public GameObject ragdoll;
+	public GameObject newRagdoll;
+
+	public void GoRagdoll() {
+		if(true) {
+
+			// Modified by Sam
+			rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+			transform.collider.enabled = false;
+			SkinnedMeshRenderer theMesh = transform.gameObject.GetComponentInChildren<SkinnedMeshRenderer>();
+			theMesh.enabled = false;
+
+			// Instantiate ragdoll
+			newRagdoll = Instantiate(ragdoll, transform.position, transform.rotation) as GameObject;
+			
+			// Copy bone transforms to ragdoll
+			CopyTransforms(transform, newRagdoll.transform);
+
+			// DO NOT destroy the dinosaur game object!
+			//Destroy(this.gameObject);
+		}
+	}
+
+	// Added by Sam
+	public void ResetRacer()
+	{
+		rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+		transform.collider.enabled = true;
+		SkinnedMeshRenderer theMesh = transform.gameObject.GetComponentInChildren<SkinnedMeshRenderer>();
+		theMesh.enabled = true;
+		ColorLerpClass theLerp = transform.gameObject.GetComponent<ColorLerpClass>();
+		theLerp.lerping = false;
+		Destroy(newRagdoll);
+	}
+
+	void CopyTransforms(Transform src, Transform dst) {
+		dst.position = src.position;
+		dst.rotation = src.rotation;
+		dst.gameObject.SetActive(src.gameObject.activeSelf);
+		
+		foreach(Transform child in dst) {
+			
+			// match the transform with the same name
+			var curSrc = src.Find(child.name);
+			
+			if (curSrc != null) 
+				CopyTransforms(curSrc, child);
+		}
+	}
+
+}
---
+++ CyberDino_Racing(GitHub)/Assets/Racers/Scripts/UserControl.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserControl : MonoBehaviour {
+
+	private DinoMoveScript move;
+
+	// Added by Sam
+	private WeaponControlClass weapons;
+	private VFXControlClass vfx;
+	private RacerHealthClass racerHealth;
+	private DustEffectClass dust;
+
+	// Use this for initialization
+	void Start () {
+		move = this.gameObject.GetComponent<DinoMoveScript>();
+
+		// Added by Sam
+		weapons = GetComponentInChildren<WeaponControlClass>();
+		vfx = GetComponentInChildren<VFXControlClass>();
+		racerHealth = GetComponentInChildren<RacerHealthClass>();
+		racerHealth.TheRagdoll = this.GetComponent<DinoRagdoll>();
+		dust = GetComponentInChildren<DustEffectClass>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		move.h = Input.GetAxis("Horizontal");				
+		move.v = Input.GetAxis("Vertical");
+		if(Input.GetButton("Jump"))
+		{
+			move.jumper = true;
+		}
+		else
+		{
+			move.jumper = false;
+		}
+
+//		if(Input.GetKeyDown(KeyCode.F))
+//		{
+//			this.GetComponent<DinoRagdoll>().GoRagdoll();
+//		}
+
+
+		// Added by Sam
+		dust.RunDust(move.v);
+
+		if(Input.GetKeyUp(KeyCode.F))
+		{
+			if(!racerHealth.IsRagdoll)
+			{
+				this.GetComponent<DinoRagdoll>().GoRagdoll();
+				racerHealth.IsRagdoll = true;
+			}
+			else
+			{
+				this.GetComponent<DinoRagdoll>().ResetRacer();
+				racerHealth.IsRagdoll = false;
+			}
+		}
+		
+		if(Input.GetKeyUp(KeyCode.T))
+		{
+			vfx.UseTurbo();
+		}
+		if(Input.GetKeyUp(KeyCode.Alpha1))
+		{
+			weapons.Switch();
+		}
+		if(Input.GetKeyUp(KeyCode.R))
+		{
+			racerHealth.UseRespawn(gameObject);
+		}
+		if(Input.GetKey(KeyCode.Q))
+		{
+			switch(weapons.CurrentWeapon.name.ToString())
+			{
+			case "Laser_VFX":
+				weapons.Laser.FireLaser();
+				break;
+			case "MachineGun_Gun":
+				weapons.OldMachineGun.FireFunc();
+				break;
+			case "PlayerMissileLauncher":
+				weapons.MissileLauncher.FireFunc();
+				break;
+			default:
+				break;
+			}
+		}
+		else
+		{
+			weapons.Laser.Line.enabled = false;
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		
+		string theTag = other.gameObject.tag.ToString();
+		
+		switch(theTag)
+		{
+		case "Weapon":
+//			Debug.Log (this + " has been hit with a weapon!");
+			break;
+//		case "DirectionCheck":
+//			if(!InTurnArea)
+//			{
+//				InTurnArea = true;
+//			}
+//			else
+//			{
+//				InTurnArea = false;
+//			}
+//			break;
+		default:
+			break;
+		}
+		
+	}
+
+}
